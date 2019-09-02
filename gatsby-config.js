809924fb2235e6ffbcc1bf7f840cc536408f3a5f
Oliver Heward <oliverhewardbie@gmail.com>
2019-09-02 16:49:26 +0100

    trying again

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
          * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: "https://wp.hewy.dev",
+        baseUrl: "wp.hewy.dev",
         // The protocol. This can be http or https.
         protocol: "https",
         // Indicates whether the site is hosted on wordpress.com.
