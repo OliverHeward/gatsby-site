46a9b60794e505fea4c106df8b9457ff23cc2fda
Oliver Heward <oliverhewardbie@gmail.com>
2019-09-02 16:42:32 +0100

    changing gatsby-source-wordpress resolve to point to wp subdomain

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,9 +34,9 @@ module.exports = {
          * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: "localhost:8888/gatsby-react",
+        baseUrl: "https://wp.hewy.dev",
         // The protocol. This can be http or https.
-        protocol: "http",
+        protocol: "https",
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
